77176af63336c2e54bb4540c58c5b14547a8a13d
Cesar <cramirez8@ucol.mx>
2021-06-15 18:42:23 -0500

    orders-1

--- lib/screens/orders.dart
+++ lib/screens/orders.dart
@@ -1,1 +1,12 @@
-//xd
+import 'package:flutter/material.dart';
+
+class Orders extends StatelessWidget {
+  const Orders({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      child: null,
+    );
+  }
+}

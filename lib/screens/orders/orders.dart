2e9981ce405188473c3a8ae2a73df62c2d62931e
Cesar <cramirez8@ucol.mx>
2021-10-15 02:33:04 -0500

    ya casi xd

--- lib/screens/orders/orders.dart
+++ lib/screens/orders/orders.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:smart_order/screens/map/map.dart';
 
 class Orders extends StatefulWidget {
   const Orders({Key? key}) : super(key: key);
@@ -19,88 +20,138 @@ class _OrdersState extends State<Orders> {
 
   Future getOrders() async {
     await Firebase.initializeApp();
-    DocumentSnapshot seller =
-        await FirebaseFirestore.instance.collection('orders').doc('3121811727').get();
-      if(mounted){
-        if(seller.exists){
-         setState(() {
-            orders = seller['orders'];
-         });
-        } 
+    DocumentSnapshot seller = await FirebaseFirestore.instance
+        .collection('orders')
+        .doc('3121811727')
+        .get();
+    if (mounted) {
+      if (seller.exists) {
+        setState(() {
+          orders = seller['orders'];
+        });
       }
+    }
+  }
 
+  Future setStatus(
+      {required String id, required int index, required bool accepted}) async {
+    print(index);
+    await Firebase.initializeApp();
+    DocumentSnapshot documentOrders =
+        await FirebaseFirestore.instance.collection('orders').doc(id).get();
+    //documentOrders['orders'][index]['state']['accepted'] = true;
+    List orders = documentOrders['orders'];
+    orders[index]['state']['accepted'] = accepted;
+    //print(documentOrders['orders'][index]['state']['accepted']);
+    await FirebaseFirestore.instance
+        .collection("orders")
+        .doc(id)
+        .update({'orders': orders});
   }
 
   @override
   Widget build(BuildContext context) {
-            double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
         body: SingleChildScrollView(
       child: SafeArea(
-        child: Wrap(
-        children: orders
-            .map(
-              (order) => GestureDetector(
-                onTap: () {
-                 
-                },
-                child: Container(
-                  width: width,
-                  child: Card(
-                      child: Row(
-                    children: [
-                      Expanded(
-                        flex: 4,
-                        child: Container(
-                          margin: EdgeInsets.all(12),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.network(
-                              order['food']['image'],
-                              width: 200,
-                              height: 115,
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 5,
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          child: Wrap(
+              children: orders
+                  .map(
+                    (order) => GestureDetector(
+                      onTap: () {},
+                      child: Container(
+                        width: width,
+                        child: Card(
+                            child: Row(
                           children: [
-                            Text("${order['food']['title']}",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 22)),
-                            Text("${order['food']['description']}",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w400,
-                                    fontSize: 18)),
-                            SizedBox(
-                              height: 15,
-                            ),
-                            Text(
-                              "\$${order['food']['price'].toDouble()}",
-                              style: TextStyle(
-                                color: Colors.orange,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                            Expanded(
+                              flex: 4,
+                              child: Container(
+                                margin: EdgeInsets.all(12),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(12),
+                                  child: Image.network(
+                                    order['food']['image'],
+                                    width: 200,
+                                    height: 115,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
                               ),
                             ),
+                            Expanded(
+                              flex: 5,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Text("${order['food']['title']}",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 22)),
+                                  Text("${order['food']['description']}",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w400,
+                                          fontSize: 18)),
+                                  SizedBox(
+                                    height: 15,
+                                  ),
+                                  Text(
+                                    "\$${order['food']['price'].toDouble()}",
+                                    style: TextStyle(
+                                      color: Colors.orange,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            ClipOval(
+                              child: Material(
+                                color: Colors.green[200], // Button color
+                                child: InkWell(
+                                  splashColor: Colors.grey, // Splash color
+                                  onTap: () {
+                                    setStatus(
+                                        id: '3121047740',
+                                        index: orders.indexOf(order),
+                                        accepted: true);
+                                  },
+                                  child: SizedBox(
+                                      width: 56,
+                                      height: 56,
+                                      child: Icon(Icons.check)),
+                                ),
+                              ),
+                            ),
+                            ClipOval(
+                              child: Material(
+                                color: Colors.redAccent[200], // Button color
+                                child: InkWell(
+                                  splashColor: Colors.grey, // Splash color
+                                  onTap: () {
+                                    setStatus(
+                                        id: '3121047740',
+                                        index: orders.indexOf(order),
+                                        accepted: false);
+                                  },
+                                  child: SizedBox(
+                                      width: 56,
+                                      height: 56,
+                                      child: Icon(Icons.delete)),
+                                ),
+                              ),
+                            )
                           ],
-                        ),
-                      )
-                    ],
-                  )),
-                ),
-              ),
-            )
-            .toList())
-      ),
+                        )),
+                      ),
+                    ),
+                  )
+                  .toList())),
     ));
   }
 }
-
